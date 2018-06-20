16a6725ab88f1063e6c1f3b8a898c9bb6f7e41bd
Pierre-Alexandre FIALE <afiale@agaetis.fr>
2018-06-20 09:24:40 +0200

    fix discussions

--- src/main/java/fr/agaetis/monobot/model/mongo/Picture.java
+++ src/main/java/fr/agaetis/monobot/model/mongo/Picture.java
@@ -1,58 +1,18 @@
 package fr.agaetis.monobot.model.mongo;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Data
+@EqualsAndHashCode
 @Document(collection = "picture")
 public class Picture {
-
     @Id
-    private String id;
-    private String author;
-    @Indexed(unique = true)
     private String url;
+    private String author;
     private Number score;
     private String shapeObject;
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Number getScore() {
-        return this.score;
-    }
-
-    public void setScore(Number score) {
-        this.score = score;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getShapeObject() {
-        return this.shapeObject;
-    }
-
-    public void setShapeObject(String shapeObject) {
-        this.shapeObject = shapeObject;
-    }
-
-    public String getAuthor() {
-        return this.author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
--- src/main/java/fr/agaetis/monobot/service/FlickRService.java
+++ src/main/java/fr/agaetis/monobot/service/FlickRService.java
@@ -1,30 +1,81 @@
 package fr.agaetis.monobot.service;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotoList;
+import fr.agaetis.monobot.model.mongo.Picture;
+import fr.agaetis.monobot.repository.mongo.PictureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class FlickRService {
 
-    private static Flickr flickr = null;
-    @Value("${app.flickr.groups}")
-    private String[] groups;
+    @Autowired
+    private PictureRepository pictureRepository;
+    private Flickr flickr;
+    @Value("#{'${app.flickr.groups}'.split(',')}")
+    private List<String> groups;
     @Value("${app.api.key}")
     private String API_KEY;
     @Value("${app.api.secret}")
     private String API_SECRET;
 
-    public Flickr getInstance() {
-        if (null == flickr) {
-            flickr = new Flickr(this.API_KEY, this.API_SECRET, new REST());
-        }
+    private String saveInDatabase(Photo photo) {
+        String url = getUrlForPhoto(photo);
+        Picture picture = new Picture();
+        picture.setUrl(url);
+        picture.setAuthor(photo.getOwner().getUsername());
 
-        return flickr;
+        pictureRepository.save(picture);
+
+        return url;
     }
 
-    public String[] getGroups() {
+    private String getUrlForPhoto(Photo photo) {
+        return String.format(
+            "https://farm%s.staticflickr.com/%s/%s_%s.jpg",
+            photo.getFarm(),
+            photo.getServer(),
+            photo.getId(),
+            photo.getSecret()
+        );
+    }
+
+    private boolean isPhotoCanSaved(Photo photo) {
+        return !pictureRepository.findByUrl(getUrlForPhoto(photo)).isPresent();
+    }
+
+    public List<String> getPhotosFromGroup(String groupId) {
+        Integer page = 0;
+        PhotoList<Photo> photos;
+
+        try {
+            photos = flickr.getPoolsInterface().getPhotos(groupId, null, 15, page);
+        } catch (FlickrException e) {
+            return new ArrayList<>();
+        }
+
+        return photos.stream()
+            .filter(this::isPhotoCanSaved)
+            .map(this::saveInDatabase)
+            .collect(Collectors.toList());
+    }
+
+    @PostConstruct
+    public void init() {
+        flickr = new Flickr(this.API_KEY, this.API_SECRET, new REST());
+    }
+
+    public List<String> getGroups() {
         return this.groups;
     }
 }
