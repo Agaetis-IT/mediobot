54c326637f7f4a11d4502996020b06bdd54d680b
Pierre PIRONIN <pironin.pierre@gmail.com>
2018-07-27 11:33:46 +0200

    Add a Picture view specialized for Detectobot calling (fix)

--- src/main/java/fr/agaetis/mediobot/model/mongo/Picture.java
+++ src/main/java/fr/agaetis/mediobot/model/mongo/Picture.java
@@ -1,6 +1,7 @@
 package fr.agaetis.mediobot.model.mongo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +14,7 @@ import java.util.List;
 @Setter
 @ToString
 @EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Picture {
     @Id
     private String id;
