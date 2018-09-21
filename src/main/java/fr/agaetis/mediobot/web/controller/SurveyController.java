ecd1c0de7180e27ad26ce648effc5ad53f119027
Pierre PIRONIN <pironin.pierre@gmail.com>
2018-09-21 13:05:47 +0200

    Updates .gitignore

--- src/main/java/fr/agaetis/mediobot/web/controller/SurveyController.java
+++ src/main/java/fr/agaetis/mediobot/web/controller/SurveyController.java
@@ -2,8 +2,6 @@ package fr.agaetis.mediobot.web.controller;
 
 import fr.agaetis.mediobot.model.mongo.Survey;
 import fr.agaetis.mediobot.service.MongoService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/survey")
 public class SurveyController {
-    private static final Logger logger = LoggerFactory.getLogger(SurveyController.class);
-
     @Autowired
     private MongoService mongoService;
 
